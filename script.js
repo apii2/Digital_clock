143a7d8d1fe112c89c86b952538640b0aa981ebe
apeksha <apekshrestha22@gmail.com>
2024-10-20 22:59:55 +0545

    background image transition added

--- script.js
+++ script.js
@@ -12,32 +12,65 @@ function generateDate(){
 
   let greeting = "";
   let cls;
-  let ampm = document.querySelector('#ampm');
+  const ampm = document.querySelector('#ampm');
 
   if (h >= 0 && h < 12) {
     greeting = "morning";
     ampm.innerHTML = "AM";
     cls = 'fa-moon';
+
   } else if (h >= 12 && h < 17) {
     greeting = "afternoon";
     ampm.innerHTML = "PM";
     cls = 'fa-moon';
+
   } else if (h >= 17 && h < 21) {
     greeting = "evening";
     ampm.innerHTML = "PM";
     cls = 'fa-sun';
+
   } else if (h >= 21 && h <= 23) {
     greeting = "night";
     ampm.innerHTML = "PM";
     cls = 'fa-sun';
   }
 
+  bg_animation(greeting);
+
   document.querySelector("h5 span").innerHTML = "Good " + greeting + ", ";
   document.querySelector("#hr_slot").innerHTML = hour12;
   document.querySelector("#min_slot").innerHTML = m;
   document.querySelector("#sec_slot").innerHTML = s;
-  document.querySelector('#day_symbol').classList.remove(cls);
 
-  let days = ['sunday','monday','tuesday','wednesday','thrusday','friday','saturday'];
+  const days = ['sunday','monday','tuesday','wednesday','thrusday','friday','saturday'];
   document.querySelector('#day_slot').textContent = days[d];
+}
+
+function bg_animation(greeting){
+  const main_body = document.getElementsByTagName('main')[0];
+
+  if((greeting == 'morning' || greeting == 'afternoon') && !main_body.classList.contains('morningFadeIn')){
+    if(main_body.classList.contains('eveningFadeIn')){
+      main_body.classList.remove('eveningFadeIn');
+      main_body.classList.add('eveningFadeOut');
+      setTimeout(() => {
+        main_body.classList.remove('eveningFadeOut');
+        main_body.classList.add('morningFadeIn');
+        }, 1000);
+    }else{
+      main_body.classList.add('morningFadeIn');
+    }
+
+  }else if((greeting == 'evening' || greeting == 'night') && !main_body.classList.contains('eveningFadeIn')){
+    if(main_body.classList.contains('morningFadeIn')){
+      main_body.classList.remove('morningFadeIn');
+      main_body.classList.add('morningFadeOut');
+      setTimeout(() => {
+        main_body.classList.remove('morningFadeOut');
+        main_body.classList.add('eveningFadeIn');
+        }, 1000);
+    }else{
+      main_body.classList.add('eveningFadeIn');
+    }
+  }
 }
